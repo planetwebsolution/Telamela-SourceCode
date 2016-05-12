72077266181fd24fd14bf8f9fd1d1e0412f242d9
root <root@pws-82.(none)>
2016-05-12 14:43:18 +0530

    header logistic

--- admin/inc/header_logistic.inc.php
+++ admin/inc/header_logistic.inc.php
@@ -10,7 +10,7 @@
  * ***************************** */
 $varFileName = basename($_SERVER['PHP_SELF']);
 if (!$_SESSION['sessLogistictype']) {
-   header('location: ' . SITE_ROOT_URL . 'logistic/index.php');
+    header('location: ' . SITE_ROOT_URL . 'logistic/index.php');
     exit();
 }
 //pre($varFileName);
@@ -19,23 +19,23 @@ if (!$_SESSION['sessLogistictype']) {
 <div id="navigation">
     <div class="container-fluid top_nav">
         <a href="<?php echo $_SESSION['sessUser'] != 0 ? 'dashboard_uil.php' : 'index.php'; ?>" id="brand"><img src="../admin/img/logo.png"  alt="<?php echo SITE_NAME; ?>" title="<?php echo SITE_NAME; ?>" /></a>
-        
+
         <div class="user">
-            
+
             <div class="dropdown">
                 <a href="#" class='dropdown-toggle admin' data-toggle="dropdown"><?php echo $_SESSION['sessLogisticTitle']; ?> <img src="../admin/img/admin.png" alt=""></a>
                 <ul class="dropdown-menu pull-right">
-<!--                    <li>
-                        <a class="setting" <?php echo ($varFileName == 'settings_frm_uil.php') ? 'class="active"' : 'href="settings_frm_uil.php"'; ?>>Settings</a>
-                    </li>-->
+                    <!--                    <li>
+                                            <a class="setting" <?php echo ($varFileName == 'settings_frm_uil.php') ? 'class="active"' : 'href="settings_frm_uil.php"'; ?>>Settings</a>
+                                        </li>-->
                     <?php if ($_SESSION['sessLogistictype'] == 'logistic-admin') { ?>
-<!--                         <li>
-                            <a href="shipping_method_manage_uil.php" class="setting">Shipping Method</a>
-                        </li>-->
+                        <!--                         <li>
+                                                    <a href="shipping_method_manage_uil.php" class="setting">Shipping Method</a>
+                                                </li>-->
                     <?php } ?>
-                        
-                        
-                        
+
+
+
                     <li>
                         <a href="logout.php" class="logout" alt="Logout" title="Logout">Logout</a>
                     </li>
@@ -69,7 +69,7 @@ if (!$_SESSION['sessLogistictype']) {
             </li>
 
             <?php
-            if (in_array($varFileName, array('setup_manage_uil.php', 'product_add_uil.php','zone_add_uil.php','zone_edit_uil.php','price_manage_uil.php','price_add_uil.php','price_edit_uil.php'))) {
+            if (in_array($varFileName, array('setup_manage_uil.php', 'product_add_uil.php', 'zone_add_uil.php', 'zone_edit_uil.php', 'price_manage_uil.php', 'price_add_uil.php', 'price_edit_uil.php'))) {
                 $class = 'class="active open"';
                 $display = 'style="display: block; z-index:100;"';
             } else {
@@ -81,29 +81,29 @@ if (!$_SESSION['sessLogistictype']) {
                     <span>SetUp</span>
                     <span class="caret"></span>
                 </a>
-                
+
                 <ul class="dropdown-menu" <?php echo $display; ?>>
                     <li><a href="setup_manage_uil.php" 
-                        
-                        <?php
-            if (in_array($varFileName, array('setup_manage_uil.php',  'zone_add_uil.php', 'zone_edit_uil.php'))) {
-                echo 'class="current"';
-            } else {
-               // unset($_SESSION['cat']['gcid']);
-            }
-            ?>>Manage Zone</a></li>
-            
-                    <li><a href="price_manage_uil.php" <?php
-                           if ($varFileName == 'price_manage_uil.php' || $varFileName == 'price_add_uil.php' || $varFileName == 'price_edit_uil.php') {
+
+                           <?php
+                           if (in_array($varFileName, array('setup_manage_uil.php', 'zone_add_uil.php', 'zone_edit_uil.php'))) {
                                echo 'class="current"';
+                           } else {
+                               // unset($_SESSION['cat']['gcid']);
                            }
-            ?>>Manage Price</a></li>
-                    
+                           ?>>Manage Zone</a></li>
+
+                    <?php /*<li><a href="price_manage_uil.php" <?php
+                        if ($varFileName == 'price_manage_uil.php' || $varFileName == 'price_add_uil.php' || $varFileName == 'price_edit_uil.php') {
+                            echo 'class="current"';
+                        }
+                        ?>>Manage Price</a></li>
+                        */ ?>
                 </ul>
             </li>
 
             <?php
-            if ($varFileName == 'invoic_manage_uil.php' ) {
+            if ($varFileName == 'invoic_manage_uil.php') {
                 $class = 'class="active open"';
                 $display = 'style="display: block; z-index:100;"';
             } else {
@@ -111,18 +111,18 @@ if (!$_SESSION['sessLogistictype']) {
                 $display = '';
             }
             ?>
-            <li <?php echo $class ?>>
+            <?php /*<li <?php echo $class ?>>
                 <a href="invoic_manage_uil.php" data-toggle1="dropdown" class='dropdown-toggle'>
                     <span>Invoices</span>
                     <span class="caret"></span>
                 </a>
                 <ul class="dropdown-menu" <?php echo $display; ?>>
                     <li><a href="#" <?php echo in_array($varFileName, array('invoice_detail_uil.php')) ? 'class="current"' : ''; ?>>Invoice Details</a></li>
-                     </ul>
+                </ul>
             </li>
-            
+
             <?php
-            if ($varFileName == 'logisticorder_manage_uil.php' ) {
+            if ($varFileName == 'logisticorder_manage_uil.php') {
                 $class = 'class="active open"';
                 $display = 'style="display: block; z-index:100;"';
             } else {
@@ -137,11 +137,11 @@ if (!$_SESSION['sessLogistictype']) {
                 </a>
                 <ul class="dropdown-menu" <?php echo $display; ?>>
                     <li><a href="#" <?php echo in_array($varFileName, array('logisticorder_detail_uil.php')) ? 'class="current"' : ''; ?>>Invoice Details</a></li>
-                     </ul>
-            </li>
-            
+                </ul>
+            </li>*/ ?>
+
             <?php
-            if ($varFileName == 'notification_manage_uil.php' ) {
+            if ($varFileName == 'notification_manage_uil.php') {
                 $class = 'class="active open"';
                 $display = 'style="display: block; z-index:100;"';
             } else {
@@ -157,7 +157,7 @@ if (!$_SESSION['sessLogistictype']) {
                 <ul class="dropdown-menu" <?php echo $display; ?>>
                     <li><a href="#" <?php echo in_array($varFileName, array('notifyorderupdate_uil.php')) ? 'class="current"' : ''; ?>>Order Updates</a></li>
                     <li><a href="#" <?php echo in_array($varFileName, array('notifyuserquery.php')) ? 'class="current"' : ''; ?>>Users Queries</a></li>
-                     </ul>
+                </ul>
             </li>
 
         </ul>
